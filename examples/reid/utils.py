ccc039ec49813cf65e91463a78e50168c02babb9
Flowerfan <flower.fan@foxmail.com>
2017-06-12 17:53:04 +0800

    add function

--- examples/reid/utils.py
+++ examples/reid/utils.py
@@ -1,15 +1,18 @@
-from PIL import Image
 import numpy as np
 import os
+from keras.preprocessing import image
 
-train_dire = '/Users/flowerfan/Desktop/Market-1501-v15.09.15/bounding_box_train/'
-test_dire = '/Users/flowerfan/Desktop/Market-1501-v15.09.15/bounding_box_test/'
-query_dire = '/Users/flowerfan/Desktop/Market-1501-v15.09.15/query/'
+def extract_data_from_lst(lst, preprocess=True):
+    x = []
+    for file in lst:
+        x += [read_input_img(file)]
+    x = np.array(x)
+    if preprocess:
+        x = img_process(x)
+    return x
 
 
-
-
-def extract_imgs(dire):
+def generate_train_lst(dire):
     x = []
     y = []
     cam = []
@@ -26,15 +29,29 @@ def extract_imgs(dire):
     return x,y,cam
 
 def read_input_img(file):
-    im = Image.open(file)
-    im = im.resize((256,256))
+    im = image.load_img(file, target_size=(224,224,))
+    im = image.img_to_array(im)
     return im
 
+def img_process(imgs, shift = (97.10,99.23,105.45)):
+    imgs[:,:,:,0] -= shift[0]
+    imgs[:,:,:,1] -= shift[1]
+    imgs[:,:,:,2] -= shift[2]
+   # imgs[:,:,:,0] /= 255
+   # imgs[:,:,:,1] /= 255
+   # imgs[:,:,:,2] /= 255
+    return imgs
 
-x_train, y_train, cam_train = extract_imgs(train_dire)
-x_test, y_test, cam_test = extract_imgs(test_dire)
-x_query, y_query, cam_query = extract_imgs(query_dire)
+if __name__ == '__main__':
+    train_dire = '/home/zqx/Desktop/flowerfan/data/Market-1501-v15.09.15/bounding_box_train/'
+    test_dire = '/home/zqx/Desktop/flowerfan/data/Market-1501-v15.09.15/bounding_box_test/'
+    query_dire = '/home/zqx/Desktop/flowerfan/data/Market-1501-v15.09.15/query/'
 
-np.savez('market1501.npz', x_train=x_train, y_train=y_train, cam_train=cam_train
-                         , x_test=x_test, y_test=y_test, cam_test=cam_test
-                         , x_query=x_query, y_query=y_query, cam_query=cam_query)
+
+    x_train, y_train, cam_train = generate_train_lst(train_dire)
+    x_test, y_test, cam_test = generate_train_lst(test_dire)
+    x_query, y_query, cam_query = generate_train_lst(query_dire)
+
+    np.savez('../data/train.lst', data=x_train, label=y_train, cam=cam_train)
+    np.savez('../data/test.lst', data=x_test, label=y_test, cam=cam_test)
+    np.savez('../data/query.lst', data=x_query, label=y_query, cam=cam_query)
