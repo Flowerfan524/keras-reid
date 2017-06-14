6f16cae47649e1b327e86921a92925342f305209
Flowerfan <flower.fan@foxmail.com>
2017-06-14 11:21:45 +0800

    fix bug

--- examples/reid/reid_net.py
+++ examples/reid/reid_net.py
@@ -1,18 +1,18 @@
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.layers import Dense, Dropout, Input, Lambda, Flatten
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, SGD
 from keras import backend as K
 from keras.utils import to_categorical
 
 
 def euclidean_distance(vects):
     x, y = vects
-    return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
+    return K.square(x - y)
 
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
-    return (shape1[0], 1)
+    return shape1
 
 
 def contrastive_loss(y_true, y_pred):
@@ -35,7 +35,7 @@ def reid_net(include_top = True, input_shape = None):
 #    feature = Dropout(0.2,name='drop2')(x)
 #    if not include_top:
 #        return base_model
-    feature = Dropout(0.2, name='drop1')(feature)
+#    feature = Dropout(0.2, name='drop1')(feature)
     cls_out = Dense(751,activation='softmax', name='softmax')(feature)
     cls_model = Model(vgg16.input,cls_out)
 
@@ -44,10 +44,13 @@ def reid_net(include_top = True, input_shape = None):
     fea1,fea2 = base_model(input1), base_model(input2)
     cls1,cls2 = cls_model(input1), cls_model(input2)
     distance = Lambda(euclidean_distance,
-                      output_shape=eucl_dist_output_shape)([fea1, fea2])
-    model = Model(inputs = [input1, input2], outputs = [distance,cls1,cls2])
+                      output_shape=eucl_dist_output_shape,name='distance')([fea1, fea2])
+    diff_out = Dense(1, activation='relu', name='loss_diff')(distance)
+    model = Model(inputs = [input1, input2], outputs = [diff_out,cls1,cls2])
     rms = RMSprop()
-    model.compile(loss=['binary_crossentropy','categorical_crossentropy','categorical_crossentropy'],optimizer=rms,loss_weights=[0.5,0.5,0.5])
+    sgd = SGD(lr=0.001, momentum=0.9, decay=0.0005)
+    model.compile(loss=['binary_crossentropy','categorical_crossentropy','categorical_crossentropy'],
+            optimizer=sgd,loss_weights=[0.5,0.5,0.5])
     return model
 
 
