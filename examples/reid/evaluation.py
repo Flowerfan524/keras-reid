11a6e5389da9cee0377330f4f8e89db86f8bbd8c
Flowerfan <flower.fan@foxmail.com>
2017-07-06 12:51:35 +0800

    evaluation

--- examples/reid/evaluation.py
+++ examples/reid/evaluation.py
@@ -4,7 +4,6 @@ from scipy.spatial.distance import cdist
 def compute_ap(good_idx, junk_idx, pred_idx):
     cmc = np.zeros(pred_idx.shape)
     ngood = good_idx.shape[0]
-    
     old_recall = 0.0
     old_precision = 1.0
     ap = 0
@@ -35,28 +34,51 @@ def compute_ap(good_idx, junk_idx, pred_idx):
     if good_now < good:
         raise 'something wrong'
 
+def evaluate(f1,f2,y1,y2,cam1,cam2):
+    cos_dist = cdist(f1, f2, metric='cosine')
+    cmc = np.zeros(shape=cos_dist.shape)
+    ap = np.zeros(cos_dist.shape[0])
+    for idx,cls in enumerate(y1):
+        print('processing {}/{} query file'.format(idx+1, cos_dist.shape[0]))
+        good_idx = np.intersect1d(np.where(y2 == cls)[0], 
+                np.where(cam2[idx] != cam1[idx])[0])
+        junk_idx1 = np.intersect1d(np.where(y2 == cls)[0],
+                np.where(cam2[idx] == cam1[idx])[0])
+        junk_idx2 = np.where(cam2 == -1)[0]
+        junk_idx = np.union1d(junk_idx1, junk_idx2)
+        pred_idx = np.argsort(cos_dist[idx,:])
+        ap[idx],cmc[idx,:] = compute_ap(good_idx, junk_idx, pred_idx)
+    fcmc = np.mean(cmc, axis = 0)
+    print('map:{}, r1_precision:{}'.format(np.mean(ap),fcmc[0]))
+    return None
 
-query_file = '../data/query_feature.npz'
-test_file = '../data/test_feature.npz'
+if __name__ == '__main__':
+    query_file = '../data/query_feature.npz'
+    test_file = '../data/test_feature.npz'
+    query_data = np.load(query_file)
+    test_data = np.load(test_file)
 
-query_data = np.load(query_file)
-test_data = np.load(test_file)
+    query_feature = query_data['feature']
+    test_feature = test_data['feature']
+    #query_sum = np.sum(query_data['feature']**2,axis=0)
+    #query_feature = query_data['feature']**2/query_sum[np.newaxis,:]
+    #test_sum = np.sum(test_data['feature']**2,axis=0)
+    #test_feature = test_data['feature']**2/test_sum[np.newaxis,:]
+    cos_dist = cdist(query_feature, test_feature, metric='cosine')
 
-cos_dist = cdist(query_data['feature'], test_data['feature'], metric='cosine')
+    cmc = np.zeros(shape=cos_dist.shape)
+    ap = np.zeros(cos_dist.shape[0])
 
-cmc = np.zeros(shape=cos_dist.shape)
-ap = np.zeros(cos_dist.shape[0])
-
-for idx,cls in enumerate(query_data['label']):
-    print('processing {}/{} query file'.format(idx+1, cos_dist.shape[0]))
-    good_idx = np.intersect1d(np.where(test_data['label'] == cls)[0], 
-            np.where(test_data['cam'] != query_data['cam'][idx])[0])
-    junk_idx1 = np.intersect1d(np.where(test_data['label'] == cls)[0],
-            np.where(test_data['cam'] == query_data['cam'][idx])[0])
-    junk_idx2 = np.where(test_data['label'] == -1)[0]
-    junk_idx = np.union1d(junk_idx1, junk_idx2)
-    pred_idx = np.argsort(cos_dist[idx,:])
-    ap[idx],cmc[idx,:] = compute_ap(good_idx, junk_idx, pred_idx)
-
-fcmc = np.mean(cmc, axis = 0)
-print('map:{}, r1_precision:{}'.format(np.mean(ap),fcmc[0]))
+    for idx,cls in enumerate(query_data['label']):
+        #print('processing {}/{} query file'.format(idx+1, cos_dist.shape[0]))
+        good_idx = np.intersect1d(np.where(test_data['label'] == cls)[0], 
+                np.where(test_data['cam'] != query_data['cam'][idx])[0])
+        junk_idx1 = np.intersect1d(np.where(test_data['label'] == cls)[0],
+                np.where(test_data['cam'] == query_data['cam'][idx])[0])
+        junk_idx2 = np.where(test_data['label'] == -1)[0]
+        junk_idx = np.union1d(junk_idx1, junk_idx2)
+        pred_idx = np.argsort(cos_dist[idx,:])
+        ap[idx],cmc[idx,:] = compute_ap(good_idx, junk_idx, pred_idx)
+    
+    fcmc = np.mean(cmc, axis = 0)
+    print('map:{}, r1_precision:{}'.format(np.mean(ap),fcmc[0]))
